a38f92705628a8d4b24b7dc64a0621af7f544b6f
barovski <barovski_kn@abv.bg>
2015-02-02 19:40:02 +0200

    merge all models
    models merge

--- dev/CI/application/models/interactive_content_model.php
+++ dev/CI/application/models/interactive_content_model.php
@@ -1,39 +1,39 @@
-<?php
-
-class Interactive_content_model extends CI_Model {
-
-	private $_table_cont_pages = 'content_page_slides';
-	private $_table_int_assessment = 'interactive_assessments_slides';
-
-	public function __construct() {
-		parent::__construct();
-	}
-	
-	public function get_il_content_pages($lesson_id = ''){
-		$where = array(
-			'lesson_id' => $lesson_id
-		);
-		$query = $this->db->get_where($this->_table_cont_pages, $where);
-		return $query->result();
-	}
-	
-	public function get_il_int_assesments($lesson_id = ''){
-		$where = array(
-			'lesson_id' => $lesson_id
-		);
-		$query = $this->db->get_where($this->_table_int_assessment, $where);
-		return $query->result();
-	}
-		
-	public function if_has_assesments($lesson_id = ''){
-		$where = array(
-			'lesson_id' => $lesson_id
-		);
-		$queryi = $this->db->get_where($this->_table_int_assessment, $where);
-		$queryc = $this->db->get_where($this->_table_cont_pages, $where);
-		
-		return sizeof($queryc->result() + $queryi->result()) > 0 ? true : false;
-	}
-	
-	
+<?php
+
+class Interactive_content_model extends CI_Model {
+
+	private $_table_cont_pages = 'content_page_slides';
+	private $_table_int_assessment = 'interactive_assessments_slides';
+
+	public function __construct() {
+		parent::__construct();
+	}
+	
+	public function get_il_content_pages($lesson_id = ''){
+		$where = array(
+			'lesson_id' => $lesson_id
+		);
+		$query = $this->db->get_where($this->_table_cont_pages, $where);
+		return $query->result();
+	}
+	
+	public function get_il_int_assesments($lesson_id = ''){
+		$where = array(
+			'lesson_id' => $lesson_id
+		);
+		$query = $this->db->get_where($this->_table_int_assessment, $where);
+		return $query->result();
+	}
+		
+	public function if_has_assesments($lesson_id = ''){
+		$where = array(
+			'lesson_id' => $lesson_id
+		);
+		$queryi = $this->db->get_where($this->_table_int_assessment, $where);
+		$queryc = $this->db->get_where($this->_table_cont_pages, $where);
+		
+		return sizeof($queryc->result() + $queryi->result()) > 0 ? true : false;
+	}
+	
+	
 }
