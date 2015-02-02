8456399e897635b3eec1470353d8a5ee92853cfc
barovski <barovski_kn@abv.bg>
2015-02-02 19:45:57 +0200

    Revert "Revert dcba293..a38f927"
    This reverts commit d53da87ac2de28d9465d4abd000d431d43b9fd54.

--- dev/CI/application/models/interface_model.php
+++ dev/CI/application/models/interface_model.php
@@ -1,6 +1,6 @@
-<?php
-class Interface_model extends CI_Model {
-    private $_prefix = 'nc_';    private $_lang_table = 'lang_content';
-    public function __construct() {        parent::__construct();    }
-    public function get_lang_data($table_name, $field_name, $lang_id) {        $where = array(            'object_table' => $this->_prefix . $table_name,            'field_name' => $field_name,            'lang_id' => $lang_id        );        $this->db->where($where);        $query = $this->db->get($this->_lang_table);        return $query->result();    }
+<?php
+class Interface_model extends CI_Model {
+    private $_prefix = 'nc_';    private $_lang_table = 'lang_content';
+    public function __construct() {        parent::__construct();    }
+    public function get_lang_data($table_name, $field_name, $lang_id) {        $where = array(            'object_table' => $this->_prefix . $table_name,            'field_name' => $field_name,            'lang_id' => $lang_id        );        $this->db->where($where);        $query = $this->db->get($this->_lang_table);        return $query->result();    }
 }
