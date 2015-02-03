58ff2716abf703cbfcac92bb0a8a0e9951a26c7a
mkdgo <dimitar.ovcharov70@gmail.com>
2015-02-03 12:16:29 +0200

    added test function

--- dev/CI/application/controllers/e1_teacher.php
+++ dev/CI/application/controllers/e1_teacher.php
@@ -1,5 +1,5 @@
 <?php
-
+//mkdgo
 if (!defined('BASEPATH'))
 	exit('No direct script access allowed');
 
@@ -17,6 +17,7 @@ class E1_teacher extends MY_Controller {
         $this->load->library( 'nativesession' );
 	}
 
+    function test(){}
 	function index($subject_id = '',$module_id = '', $lesson_id = '') {
 
         $selected_year = $this->getSelectYearTeacher($this->nativesession, $this->subjects_model, $subject_id, '');
