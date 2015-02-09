d34915484298ba9512c6a8faf828a40472008c54
Spas Bobchev <Spas Bobchev@HOYYA-Spas>
2015-02-09 10:38:20 +0200

    Database Migrations: Revert new field "objectives_plenary" in "lessons" and "modules" tables.

--- CI/application/migrations/09-02-2015.sql
+++ CI/application/migrations/09-02-2015.sql
@@ -1,2 +1,5 @@
 ALTER TABLE `lessons` CHANGE COLUMN `objectives_plenary` `objectives_plenary` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL AFTER `objectives`;
 ALTER TABLE `modules` ADD COLUMN `objectives_plenary` TEXT NULL AFTER `objectives`;
+# REVERT #
+ALTER TABLE `lessons` DROP COLUMN `objectives_plenary`;
+ALTER TABLE `modules` DROP COLUMN `objectives_plenary`;
