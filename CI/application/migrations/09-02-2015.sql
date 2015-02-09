a287a96cb810787944a0b623a9710d52134c6c25
Spas Bobchev <Spas Bobchev@HOYYA-Spas>
2015-02-09 10:56:06 +0200

    Database Migrations: - new "key_words_objectives" table; - new "key_words_lessons" table; - new "key_words_modules" table.

--- CI/application/migrations/09-02-2015.sql
+++ CI/application/migrations/09-02-2015.sql
@@ -3,3 +3,19 @@ ALTER TABLE `modules` ADD COLUMN `objectives_plenary` TEXT NULL AFTER `objective
 # REVERT #
 ALTER TABLE `lessons` DROP COLUMN `objectives_plenary`;
 ALTER TABLE `modules` DROP COLUMN `objectives_plenary`;
+
+CREATE TABLE `key_words_objectives` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `word` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `key_words_lessons` (
+  `key_word` int(11) NOT NULL,
+  `lesson` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `key_words_modules` (
+  `key_word` int(11) NOT NULL,
+  `module` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
