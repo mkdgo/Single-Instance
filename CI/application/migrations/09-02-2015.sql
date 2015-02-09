929be94f3145bbcc6d21824b863eb76288c57124
Spas Bobchev <Spas Bobchev@HOYYA-Spas>
2015-02-09 10:21:05 +0200

    Database Migrations: Add new field "objectives_plenary" to "modules" table.

--- CI/application/migrations/09-02-2015.sql
+++ CI/application/migrations/09-02-2015.sql
@@ -1,1 +1,2 @@
 ALTER TABLE `lessons` CHANGE COLUMN `objectives_plenary` `objectives_plenary` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL AFTER `objectives`;
+ALTER TABLE `modules` ADD COLUMN `objectives_plenary` TEXT NULL AFTER `objectives`;
