546862fdcc7a3e0f5aa4458fe0b49dd4892d50cd
Spas Bobchev <Spas Bobchev@HOYYA-Spas>
2015-02-10 10:55:04 +0200

    Database Migrations: - new "plenaries" table; - new "plenary_grade_labels" table and data; - new "plenary_grid" table.

---
+++ CI/application/migrations/10-02-2015.sql
@@ -0,0 +1,30 @@
+CREATE TABLE `plenaries` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `plenary_type` ENUM('module','lesson') NOT NULL,
+  `fk_id` INT NOT NULL COMMENT 'Foreign key to either \'modules\' or \'lessons\'.',
+  PRIMARY KEY (`id`));
+
+CREATE TABLE `plenary_grade_labels` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `label` varchar(45) NOT NULL,
+  `label_rank` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `label_UNIQUE` (`label`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `plenary_grade_labels` (`label`, `label_rank`) VALUES ('Strongly Disagree', '1');
+INSERT INTO `plenary_grade_labels` (`label`, `label_rank`) VALUES ('Disagree', '2');
+INSERT INTO `plenary_grade_labels` (`label`, `label_rank`) VALUES ('Neutral', '3');
+INSERT INTO `plenary_grade_labels` (`label`, `label_rank`) VALUES ('Agree', '4');
+INSERT INTO `plenary_grade_labels` (`label`, `label_rank`) VALUES ('Strongly Agree', '5');
+
+CREATE TABLE `plenary_grid` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plenary_id` int(11) NOT NULL,
+  `label_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_label_id_idx` (`label_id`),
+  CONSTRAINT `fk_label_id` FOREIGN KEY (`label_id`) REFERENCES `plenary_grade_labels` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `fk_plenary_id` FOREIGN KEY (`id`) REFERENCES `plenaries` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
