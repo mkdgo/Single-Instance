9cecd6d81fcc6a68ba8d56ce62aa255b00fee78a
barovski <barovskikn@gmail.com>
2015-04-22 14:36:43 +0300

    f4t

--- CI/application/views/_default.php
+++ CI/application/views/_default.php
@@ -4,9 +4,8 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta charset="utf-8">
         <title>EDIFACE - <?=$GLOBALS['SCHOOL']['TITLE']?></title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1.0, user-scalable=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="apple-mobile-web-app-capable" content="yes">
-        <meta name="HandheldFriendly" content="true" />
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600' rel='stylesheet' type='text/css'>
         <!-- Bootstrap -->
         <link href="<?=base_url("/css/bootstrap.css")?>" rel="stylesheet" media="screen">
