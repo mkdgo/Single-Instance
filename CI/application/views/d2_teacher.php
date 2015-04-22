d16b8f1aa2b4699ee52fe160c92c4b6eb8106642
barovski <barovskikn@gmail.com>
2015-04-22 14:44:25 +0300

    f4t

--- CI/application/views/d2_teacher.php
+++ CI/application/views/d2_teacher.php
@@ -1,9 +1,11 @@
 <link rel="stylesheet" href="<?=base_url("/css/d2_teacher.css")?>" type="text/css"/>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css" />
-<script src="http://code.jquery.com/ui/1.10.4/jquery-ui.min.js"></script>
-<script src="<?=base_url("/js/jquery.mjs.nestedSortable.js")?>"></script>
-<script src="<?=base_url("/js/d2_teacher.js")?>"></script>
+<script type="text/javascript" src="http://code.jquery.com/ui/1.10.4/jquery-ui.min.js"></script>
+<script type="text/javascript" src="https://raw.githubusercontent.com/furf/jquery-ui-touch-punch/master/jquery.ui.touch-punch.min.js"></script>
+<script type="text/javascript" src="<?=base_url("/js/jquery.mjs.nestedSortable.js")?>"></script>
+<script type="text/javascript" src="<?=base_url("/js/d2_teacher.js")?>"></script>
+
 <div class="blue_gradient_bg">
     <div class="breadcrumb_container">
         <div class="container">{breadcrumb}</div>
@@ -18,12 +20,16 @@
                         <table style="margin-bottom:0px; margin-top:20px;" class="table2">
                             <thead>
                                 <tr>
-                                    <td style="width: 100%;float:left;">
-                                        <i style="margin-right:10px;" class="icon-move"><img class="disable_dd" src="/img/icon-arrows.png"></i>
-                                        <a href="/d4_teacher/index/{subject_id}/{module_id}">{module_name}</a>
+                                    <td style="width: 100%;float:left;height:72px;position:relative;cursor: default;">
+                                        <div class="drag" style="width: 50px;height:70px;margin-top: -20px;padding-top:20px;margin-bottom:20px;text-align:center; margin-left:-14px;position:absolute;float: left">
+
+                                        <i style="margin-right:10px;cursor: move;" class="icon-move"><img  src="/img/icon-arrows.png"></i>
+                                        </div>
+                                            <a href="/d4_teacher/index/{subject_id}/{module_id}" style="padding-left: 35px;">{module_name}</a>
+
                                     </td>
-                                    <td style="width: 35%;" class="ta-c" colspan="2" style="padding-right: 60px;">Slides Available?</td>
-                                    <td style="width: 40px;"><a class="remove" href="javascript: delRequest('/d2_teacher/deleteModule/{subject_id}/{module_id}', 1,'{module_name}');"><span class="glyphicon glyphicon-remove"></span></a></td>
+                                    <td style="width: 35%;cursor: default;" class="ta-c" colspan="2" style="padding-right: 60px;">Slides Available?</td>
+                                    <td style="width: 40px;cursor: default;"><a class="remove" href="javascript: delRequest('/d2_teacher/deleteModule/{subject_id}/{module_id}', 1,'{module_name}');"><span class="glyphicon glyphicon-remove"></span></a></td>
                                 </tr>
                             </thead>
                         </table>
@@ -35,14 +41,16 @@
                                 <table style="margin-bottom:0px;" class="table2">
                                     <tbody>
                                         <tr>
-                                            <td class="first" >
-                                                <i style="margin-right:10px; padding-top: 3px;" class="icon-move"><img class="disable_dd" src="/img/icon-arrows.png"></i>
-                                                <a href="/d5_teacher/index/{subject_id}/{module_id}/{lesson_id}">
-                                                   <span style="font-style: normal;">Lesson : {lesson_title}</span>
+                                            <td class="first" style="cursor: default;position: relative;">
+                                                <div class="drag" style="width: 50px;height:70px;margin-top: -20px;padding-top:22px;margin-bottom:20px;text-align:center; margin-left:-20px;position:absolute;float: left">
+                                                <i style="cursor: move;" ><img src="/img/icon-arrows.png"></i>
+                                                </div>
+                                                    <a href="/d5_teacher/index/{subject_id}/{module_id}/{lesson_id}">
+                                                   <span style="font-style: normal;margin-left: 35px;">Lesson : {lesson_title}</span>
                                                 </a>
                                             </td>
-                                            <td class="ta-c" style="width: 35%;">{lesson_interactive}</td>
-                                            <td style="width:40px;backgroundx: black; padding-right: 20px; padding-left: 20px;"><a class="remove" href="javascript: delRequest('/d2_teacher/deleteLesson/{subject_id}/{lesson_id}', 2,'{lesson_title}');"><span class="glyphicon glyphicon-remove"></span></a></td>
+                                            <td class="ta-c" style="width: 35%;cursor: default">{lesson_interactive}</td>
+                                            <td style="width:40px;backgroundx: black;cursor: default; padding-right: 20px; padding-left: 20px;"><a class="remove" href="javascript: delRequest('/d2_teacher/deleteLesson/{subject_id}/{lesson_id}', 2,'{lesson_title}');"><span class="glyphicon glyphicon-remove"></span></a></td>
                                         </tr>
                                     </tbody>
                                 </table>
