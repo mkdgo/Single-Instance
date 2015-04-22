a2c15e8e37f30eebe78ca8d66625f9a21a09c015
barovski <barovskikn@gmail.com>
2015-04-22 16:25:14 +0300

    77 G1 - Class list page should have the title 'select student' and then a list of students underneath. It does not need a subtitle or the grey box heading

--- CI/application/views/g1_teacher_studentclass.php
+++ CI/application/views/g1_teacher_studentclass.php
@@ -4,18 +4,10 @@
     </div>
     <div class="container text-center">
         <div class="row">
-            <h2 class="pull-left">Student Search</h2>
+            <h2 class="pull-left">Select a student</h2>
         </div>
-        <div class="row">
-            <h3 class="pull-left">Select a student</h3>
-        </div>
-        <div class="row clearfix">
-            <div class="panel panel-default panel-ediface">
-                <div class="panel-heading">
-                    <h3 class="panel-title text-left"><strong>{class_name}</strong> - <em>{class_grade}/{class_subject}</em></h3>
-                </div>
-            </div>
-        </div>
+
+
         <div class="row clearfix">
             {students}
             <div class="col-sm-12 col-md-6">
