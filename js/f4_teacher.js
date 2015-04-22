48820263284082a375cbeeceed73fda14ef6449b
barovski <barovskikn@gmail.com>
2015-04-22 16:55:29 +0300

    f4t bleed

--- js/f4_teacher.js
+++ js/f4_teacher.js
@@ -646,7 +646,7 @@ function paginnation_changePage(pg) {
         IMG_holder.css('background-size', "460px "+I_height+"px");
         IMG_holder.css('background-repeat', "no-repeat");
         $("#editor").css( 'height',(I_height)+"px");
-        $("#editor_holder").css( 'height',(I_height+70)+"px");
+        $("#editor_holder").css( 'height',(I_height+90)+"px");
     });
 
     PAGIN_STRING = PAGIN_STRING.replace("%cp%", current_page+1);
