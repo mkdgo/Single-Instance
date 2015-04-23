2467a195c733eeae110c2a0dc20298303d578cf6
barovski <barovskikn@gmail.com>
2015-04-23 16:21:47 +0300

    f4t titles update

--- CI/application/views/f4_teacher.php
+++ CI/application/views/f4_teacher.php
@@ -33,9 +33,7 @@
 
 <div class="blue_gradient_bg">
     <div class="container">
-        <h2>{assignment_name}</h2>
-        <h3>{student_name}<span style=' color: #111; font-family: "Glyphicons Halflings"; font-size: 10px; margin: 0 10px;position: relative;top: -2px;'></span><span style="color:#a8a8a8;">{resource_name}</span></h3>
-        <hr class="m2">
+       <hr class="m2">
         <!--marks-->
         <div id="editor_holder" class="" style="margin-bottom: 50px;">
             <div id="editor" class="editor"></div>
