d6878e0bbafba3d6445d16ad1cd863cf5e425cc5
barovski <barovskikn@gmail.com>
2015-04-24 11:32:08 +0300

    f2b accordion

--- js/d2_teacher.js
+++ js/d2_teacher.js
@@ -236,22 +236,6 @@ $(function  () {
 
     //resizeWin();
 
-    $('.collapsed').slideUp(100)
-    $('.up_down').click(function(){
-        var next_div = $(this).next('.collapsed');
-        if($(next_div).is(":visible"))
-        {
-            $(this).next('.collapsed').slideUp(400);
-            $(this).css({'background-position':'0 1px'})
 
-        }
-        else
-        {
-            $(this).next('.collapsed').slideDown(400);
-            $(this).css({'background-position':'0 -30px'})
-
-        }
-
-    })
 
 })
--- js/main.js
+++ js/main.js
@@ -746,4 +746,21 @@ function validate_resource() {
 
         $('#saveform').submit()
     }
-}  
+}
+$(function  () {
+    $('.collapsed').slideUp(100)
+    $('.up_down').click(function () {
+        var next_div = $(this).next('.collapsed');
+        if ($(next_div).is(":visible")) {
+            $(this).next('.collapsed').slideUp(400);
+            $(this).css({'background-position': '0 1px'})
+
+        }
+        else {
+            $(this).next('.collapsed').slideDown(400);
+            $(this).css({'background-position': '0 -30px'})
+
+        }
+
+    })
+})
