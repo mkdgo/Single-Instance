00ab72e68351cd07fc56b583695bb589f6afd669
barovski <barovskikn@gmail.com>
2015-05-20 22:34:15 +0300

    #17 look and feel

--- CI/application/views/d1.php
+++ CI/application/views/d1.php
@@ -14,18 +14,18 @@
 
 
 
-		<div class="row clearfix" style="position: relative;">
+		<div class="col-lg-12 col-sm-12 col-xs-12" style="margin-top: 400px;" >
 
 			<div class="innactive_block">
-				<div class="col-lg-12 col-sm-12 col-xs-12" style="margin-top: 400px;" >
+<div class="out">
 				{na_subjects}
 				<div class="innactive_icons" >
 					<span  class="subject_icon" style="background-image: url(<?php echo base_url().'uploads/subject_icons/';?>{logo_pic});"></span>
 					<span class="subject_text_small " ">{name}</span>
 				</div>
 				{/na_subjects}
+</div>
 			</div>
-
 				</div>
 		</div>
 	</div>
