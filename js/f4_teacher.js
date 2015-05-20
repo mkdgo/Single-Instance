613563c5c3d0aa6af3ba5c553af9247e11ffb7f4
barovski <barovskikn@gmail.com>
2015-05-20 12:54:54 +0300

    trello #126

--- js/f4_teacher.js
+++ js/f4_teacher.js
@@ -673,6 +673,7 @@ function paginnation_changePage(pg) {
 
         I_height = this.height;
 
+
         $(this).remove();
         $("#preload_img").remove();
 
@@ -680,7 +681,7 @@ function paginnation_changePage(pg) {
         IMG_holder.css('background', "url('"+homeworks_html_path+img+"')");
         IMG_holder.css('background-size', "460px "+I_height+"px");
         IMG_holder.css('background-repeat', "no-repeat");
-        $("#editor").css( 'height',(I_height)+"px");
+        $("#editor").css( 'height',(I_height+40)+"px");
         $("#editor_holder").css( 'height',(I_height+100)+"px");
         $("#editor_image").css( 'height',(I_height+30)+"px");
     });
