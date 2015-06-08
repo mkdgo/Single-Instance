4597fac5fb836b7331911f968b45ae43e1e4aee3
mkdgo <dimitar.ovcharov70@gmail.com>
2015-06-08 17:42:44 +0300

    clear the running lesson of inactive teacher

--- CI/application/controllers/logout.php
+++ CI/application/controllers/logout.php
@@ -7,19 +7,23 @@ class Logout extends MY_Controller {
 
 	function __construct() {
 		parent::__construct();
-                $this->load->library('onelogin');
-                $this->config->load('onelogin');
-                
+        $this->load->library('onelogin');
+        $this->config->load('onelogin');
+        $this->load->model('lessons_model');
+
 	}
 
-	function index($logout = '') {
-	   $this->session->unset_userdata('user_type');
-	   $this->session->sess_destroy();
-	   
-           $OL_settingsInfo = $this->config->item('onelogininfo');
-           $OlAuth = $this->onelogin->OlAuth($OL_settingsInfo);
-           $OlAuth->logout();
-           
-           $this->_logout();
+    function index($logout = '') {
+        if( $this->session->userdata('user_type') == 'teacher' ) {
+            $this->lessons_model->close_running_lesson_for_teacher($this->session->userdata('id'));
+        }
+
+	    $this->session->unset_userdata('user_type');
+	    $this->session->sess_destroy();
+        $OL_settingsInfo = $this->config->item('onelogininfo');
+        $OlAuth = $this->onelogin->OlAuth($OL_settingsInfo);
+        $OlAuth->logout();
+
+        $this->_logout();
 	}	
 }
