f7b5e069e0b020cf99ae406d2edb77a4f5fbccca
barovski <barovskikn@gmail.com>
2015-06-23 16:17:27 +0300

    sync

--- CI/application/controllers/logout.php
+++ CI/application/controllers/logout.php
@@ -5,21 +5,25 @@ if (!defined('BASEPATH'))
 
 class Logout extends MY_Controller {
 
-	function __construct() {
-		parent::__construct();
-                $this->load->library('onelogin');
-                $this->config->load('onelogin');
-                
-	}
+    function __construct() {
+        parent::__construct();
+        $this->load->library('onelogin');
+        $this->config->load('onelogin');
+        $this->load->model('lessons_model');
 
-	function index($logout = '') {
-	   $this->session->unset_userdata('user_type');
-	   $this->session->sess_destroy();
-	   
-           $OL_settingsInfo = $this->config->item('onelogininfo');
-           $OlAuth = $this->onelogin->OlAuth($OL_settingsInfo);
-           $OlAuth->logout();
-           
-           $this->_logout();
-	}	
+    }
+
+    function index($logout = '') {
+        if( $this->session->userdata('user_type') == 'teacher' ) {
+            $this->lessons_model->close_running_lesson_for_teacher($this->session->userdata('id'));
+        }
+
+        $this->session->unset_userdata('user_type');
+        $this->session->sess_destroy();
+        $OL_settingsInfo = $this->config->item('onelogininfo');
+        $OlAuth = $this->onelogin->OlAuth($OL_settingsInfo);
+        $OlAuth->logout();
+
+        $this->_logout();
+    }
 }
