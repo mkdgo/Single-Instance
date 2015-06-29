844a4092078515632040f3215aeed0fb72d39139
Spas Bobchev <Spas Bobchev@HOYYA-Spas>
2015-06-29 13:46:31 +0300

    Added users to global search.

--- CI/application/controllers/s1.php
+++ CI/application/controllers/s1.php
@@ -36,161 +36,14 @@ class S1 extends MY_Controller {
     }
 
     public function query($query) {
-//        try {
-//            $index = @Zend_Search_Lucene::open(APPPATH . 'search/index');
-//            $hits = $index->find($query);
-//        } catch (Zend_Search_Lucene_Exception $ex) {
-//            $hits = array();
-//        }
-//
-//        if (count($hits) > 0) {
-//            foreach ($hits as $key => $hit) {
-////echo "<pre>";var_dump($hit->subject_id);die;
-//                // return Zend_Search_Lucene_Document object for this hit
-//                $document = $hit->getDocument();
-//                // Get the ID for the resource stored in the DB and load it:
-//                if ($hit->score >= 0) {
-//                    // Determine Search Result Type:
-//                    if ($hit->search_type == 'user') {
-////echo "<pre>";var_dump( $hit->year_id );die;
-//                        if ($hit->type == 'student') {
-//                            $this->_data['users'][$key]['name'] = $hit->name;
-//                            $this->_data['users'][$key]['type'] = $hit->type;
-//                            $this->_data['users'][$key]['id'] = $hit->user_id;
-//                            $this->_data['users'][$key]['year'] = User_model::get_student_year($hit->user_id);
-//                        }
-//                    }
-//
-//                    if ($hit->search_type == 'resource') {
-//                        if ($hit->resource_id) {
-//                            $resource = $this->resources_model->get_resource_by_id($hit->resource_id);
-//                        } else {
-//                            $resource = NULL;
-//                        }
-//                        $this->_data['resources_count'] = count($resource);
-//
-//                        $this->_data['resources'][$key] = array();
-//                        $this->_data['resources'][$key]['title'] = $document->name;
-//                        $this->_data['resources'][$key]['link'] = $document->link;
-//                        $this->_data['resources'][$key]['description'] = $document->description;
-//                        $this->_data['resources'][$key]['id'] = $hit->id;
-//                        $this->_data['resources'][$key]['type'] = $resource->type;
-//                        // Get Keywords:
-//                        try {
-//                            $this->_data['resources'][$key]['keyword'] = $hit->keyword;
-//                        } catch (exception $e) {
-//                            
-//                        }
-//
-//                        if ($resource->teacher_id) {
-//                            $teacher = $this->user_model->get_user($resource->teacher_id);
-//                        } else {
-//                            $teacher = NULL;
-//                        }
-//                        if ($teacher) {
-//                            $this->_data['resources'][$key]['user'] = $teacher->first_name . ' ' . $teacher->last_name;
-//                        } else {
-//                            $this->_data['resources'][$key]['user'] = $hit->resource_id;
-//                        }
-//
-//                        $this->_data['resources'][$key]['resource_id'] = $hit->resource_id;
-//                        $resource_object = $this->resources_model->get_resource_by_id($hit->resource_id);
-//                        $this->_data['resources'][$key]['preview'] = $this->resoucePreview($resource_object, '/c1/resource/');
-//                        $this->_data['resources_count'] = count($this->_data['resources']);
-//                    }
-//
-//                    if ($hit->search_type == 'module') {
-////echo "<pre>";var_dump($hit->subject_id);die;
-//                        if ($this->session->userdata('user_type') == 'student') {
-//                            $t = $this->subjects_model->get_student_subject_years($this->session->userdata('student_year'));
-//
-//                            $exp = explode(',', $t['subs']);
-//                            if (!in_array($hit->year_id, $exp)) {
-//                                $this->_data['modules'][$key]['name'] = $hit->name;
-//                                $this->_data['modules'][$key]['module_id'] = $hit->module_id;
-//                                $this->_data['modules'][$key]['intro'] = $hit->intro;
-//                                $this->_data['modules'][$key]['objectives'] = $hit->objectives;
-//                                $this->_data['modules'][$key]['teaching_activities'] = $hit->teaching_activities;
-//                                $this->_data['modules'][$key]['assessment_opportunities'] = $hit->assessment_opportunities;
-//                                $this->_data['modules'][$key]['notes'] = $hit->notes;
-//                                $this->_data['modules'][$key]['publish'] = $hit->publish;
-//                                $this->_data['modules'][$key]['active'] = $hit->active;
-//                                $this->_data['modules'][$key]['subject_id'] = $hit->subject_id;
-//                                $this->_data['modules'][$key]['subject_logo'] = Subjects_model::get_subject_logo($hit->subject_id);
-//                                $this->_data['modules'][$key]['subject_title'] = substr(Subjects_model::get_subject_logo($hit->subject_id), 0, -4);
-//                                $this->_data['modules'][$key]['year_id'] = $hit->year_id;
-//                                $this->_data['modules'][$key]['type'] = 'Module';
-//
-//                                $this->_data['modules_count'] = count($this->_data['modules']);
-//                            }
-//                        } else {
-//                            $this->_data['modules'][$key]['name'] = $hit->name;
-//                            $this->_data['modules'][$key]['module_id'] = $hit->module_id;
-//                            $this->_data['modules'][$key]['intro'] = $hit->intro;
-//                            $this->_data['modules'][$key]['objectives'] = $hit->objectives;
-//                            $this->_data['modules'][$key]['teaching_activities'] = $hit->teaching_activities;
-//                            $this->_data['modules'][$key]['assessment_opportunities'] = $hit->assessment_opportunities;
-//                            $this->_data['modules'][$key]['notes'] = $hit->notes;
-//                            $this->_data['modules'][$key]['publish'] = $hit->publish;
-//                            $this->_data['modules'][$key]['active'] = $hit->active;
-//                            $this->_data['modules'][$key]['subject_id'] = $hit->subject_id;
-//                            $this->_data['modules'][$key]['subject_logo'] = Subjects_model::get_subject_logo($hit->subject_id);
-//                            $this->_data['modules'][$key]['subject_title'] = substr(Subjects_model::get_subject_logo($hit->subject_id), 0, -4);
-//                            $this->_data['modules'][$key]['year_id'] = $hit->year_id;
-//                            $this->_data['modules'][$key]['type'] = 'Module';
-//                            $this->_data['modules_count'] = count($this->_data['modules']);
-//                        }
-//                    }
-//
-//                    if ($hit->search_type == 'lesson') {
-//                        //get modules
-//                        if ($this->session->userdata('user_type') == 'student') {
-//                            $modules = $this->subjects_model->get_allowed_modules_for_student($this->session->userdata('student_year'));
-//                            if ($modules) {
-//                                $dump = explode(',', $modules['l_id']);
-//                            }
-//
-//                            if (!in_array($hit->lesson_id, $dump)) {
-//                                $this->_data['lessons'][$key]['title'] = $hit->title;
-//                                $this->_data['lessons'][$key]['module_id'] = $hit->module_id;
-//                                $this->_data['lessons'][$key]['teacher_id'] = $hit->teacher_id;
-//                                $this->_data['lessons'][$key]['lesson_id'] = $hit->lesson_id;
-//                                $this->_data['lessons'][$key]['subject_id'] = $hit->subject_id;
-//                                $this->_data['lessons'][$key]['subject_logo'] = Subjects_model::get_subject_logo($hit->subject_id);
-//                                $this->_data['lessons'][$key]['subject_title'] = substr(Subjects_model::get_subject_logo($hit->subject_id), 0, -4);
-//                                $this->_data['lessons'][$key]['intro'] = $hit->intro;
-//                                $this->_data['lessons'][$key]['objectives'] = $hit->objectives;
-//                                $this->_data['lessons'][$key]['teaching_activities'] = $hit->teaching_activities;
-//                                $this->_data['lessons'][$key]['assessment_opportunities'] = $hit->assessment_opportunities;
-//                                $this->_data['lessons'][$key]['type'] = 'Lesson';
-//                            }
-//                        } else {
-//                            $this->_data['lessons'][$key]['title'] = $hit->title;
-//                            $this->_data['lessons'][$key]['module_id'] = $hit->module_id;
-//                            $this->_data['lessons'][$key]['teacher_id'] = $hit->teacher_id;
-//                            $this->_data['lessons'][$key]['subject_id'] = $hit->subject_id;
-//                            $this->_data['lessons'][$key]['subject_logo'] = Subjects_model::get_subject_logo($hit->subject_id);
-//                            $this->_data['lessons'][$key]['subject_title'] = substr(Subjects_model::get_subject_logo($hit->subject_id), 0, -4);
-//                            $this->_data['lessons'][$key]['lesson_id'] = $hit->lesson_id;
-//                            $this->_data['lessons'][$key]['intro'] = $hit->intro;
-//                            $this->_data['lessons'][$key]['objectives'] = $hit->objectives;
-//                            $this->_data['lessons'][$key]['teaching_activities'] = $hit->teaching_activities;
-//                            $this->_data['lessons'][$key]['assessment_opportunities'] = $hit->assessment_opportunities;
-//                            $this->_data['lessons'][$key]['type'] = 'Lesson';
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//        //print_r($modules);
-//        //die();
-
         $this->_data['resources'] = $this->findResourcesInElastic($query);
         $this->_data['resources_count'] = count($this->_data['resources']);
         $this->_data['modules'] = $this->findModulesInElastic($query);
         $this->_data['modules_count'] = count($this->_data['modules']);
         $this->_data['lessons'] = $this->findLessonsInElastic($query);
         $this->_data['lessons_count'] = count($this->_data['lessons']);
+        $this->_data['users'] = $this->findStudentsInElastic($query);
+        $this->_data['users_count'] = count($this->_data['users']);
 
         return $this->_data;
     }
@@ -408,7 +261,7 @@ class S1 extends MY_Controller {
             $lesson_id = $result->getParam('_id');
             $lesson = $this->lessons_model->get_lesson($lesson_id);
             $subject_id = $this->modules_model->get_module_subject($lesson->module_id);
-            
+
             if ($lesson) {
                 $lessons[$lesson_id] = array();
                 $lessons[$lesson_id]['title'] = $lesson->title;
@@ -507,6 +360,42 @@ class S1 extends MY_Controller {
         }
 
         return $resources;
+    }
+
+    private function findStudentsInElastic($query) {
+        $this->load->model('settings_model');
+
+        $host = $this->settings_model->getSetting('elastic_url');
+        $client = new \Elastica\Client(array(
+            'host' => $host
+        ));
+
+        $search = new \Elastica\Search($client);
+        $search->addIndex($this->settings_model->getSetting('elastic_index'))->addType('students');
+
+        $nameQuery = new \Elastica\Query\Match();
+        $nameQuery->setField('fullname', array(
+            'query' => trim($query)
+        ));
+
+        $search->setQuery($nameQuery);
+
+        $results = $search->search();
+
+        $students = array();
+        foreach ($results->getResults() as $result) {
+            $source = $result->getParam('_source');
+            $student_id = $source['id'];
+            $student_name = $source['fullname'];
+
+            $students[$student_id] = array();
+            $students[$student_id]['name'] = $student_name;
+            $students[$student_id]['type'] = 'student';
+            $students[$student_id]['id'] = $student_id;
+            $students[$student_id]['year'] = User_model::get_student_year($student_id);
+        }
+
+        return $students;
     }
 
 }
