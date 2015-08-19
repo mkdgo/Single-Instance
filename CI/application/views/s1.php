e8e14d90be4104644d368e4f1e5e9106584b91b5
ibeakoh <ibe@ediface.org>
2015-08-19 17:40:20 +0100

    Update s1.php

--- CI/application/views/s1.php
+++ CI/application/views/s1.php
@@ -18,14 +18,14 @@
             <!-- <div class='returned_results'>{results}</div> -->
 
             <ul class="nav nav-pills " role="tablist">
-                <li role="presentation" class="active" ><a href="#all_tabs" aria-controls="all_tab" role="tab" data-toggle="tab">All</a></li>
-                <li role="presentation" ><a href="#modules_tab" aria-controls="modules_tab" role="tab" data-toggle="tab">Modules ({modules_count})</a></li>
-                <li role="presentation" ><a href="#lessons_tab" aria-controls="lessons_tab" role="tab" data-toggle="tab">Lessons ({lessons_count})</a></li>
-                <li role="presentation" ><a href="#resources_tab" aria-controls="resources_tab" role="tab" data-toggle="tab">Resources ({resources_count})</a></li>
+                <li role="presentation<?php if($this->session->userdata('user_type') != 'teacher') {echo "_stud"} ?>" class="active" ><a href="#all_tabs" aria-controls="all_tab" role="tab" data-toggle="tab">All</a></li>
+                <li role="presentation<?php if($this->session->userdata('user_type') != 'teacher') {echo "_stud"} ?>" ><a href="#modules_tab" aria-controls="modules_tab" role="tab" data-toggle="tab">Modules ({modules_count})</a></li>
+                <li role="presentation<?php if($this->session->userdata('user_type') != 'teacher') {echo "_stud"} ?>" ><a href="#lessons_tab" aria-controls="lessons_tab" role="tab" data-toggle="tab">Lessons ({lessons_count})</a></li>
+                <li role="presentation<?php if($this->session->userdata('user_type') != 'teacher') {echo "_stud"} ?>" ><a href="#resources_tab" aria-controls="resources_tab" role="tab" data-toggle="tab">Resources ({resources_count})</a></li>
                 <?php if($this->session->userdata('user_type') == 'teacher'): ?>
                 <li role="presentation" ><a href="#students_tab" aria-controls="students_tab" role="tab" data-toggle="tab">Students ({users_count})</a></li>
                 <?php endif; ?>
-                <li role="presentation" ><a href="#works_tab" aria-controls="works_tab" role="tab" data-toggle="tab">Work (0)</a></li>
+                <li role="presentation<?php if($this->session->userdata('user_type') != 'teacher') {echo "_stud"} ?>" ><a href="#works_tab" aria-controls="works_tab" role="tab" data-toggle="tab">Work (0)</a></li>
             </ul>
 
 
