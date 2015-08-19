dbde95cf77269a5e5e590173fa661ad4ad37a57f
ibeakoh <ibe@ediface.org>
2015-08-19 15:43:12 +0100

    Update g1_teacher_student.php

--- CI/application/views/g1_teacher_student.php
+++ CI/application/views/g1_teacher_student.php
@@ -11,7 +11,7 @@
         <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                 <h2 class="pull-left">{first_name} {last_name}</h2>
-                <?php print_r($this->_data['count_assignments']); ?>
+                <?php print_r($this->_data); ?>
             </div>
         </div>
         <div class="row hidden-xs">&nbsp;</div>
