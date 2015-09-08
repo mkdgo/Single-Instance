7fbda462e0728803e9973ee1355e222ea21dd935
ibeakoh <ibe@ediface.org>
2015-09-08 03:41:13 +0100

    Update g1_teacher.php

--- CI/application/controllers/g1_teacher.php
+++ CI/application/controllers/g1_teacher.php
@@ -57,7 +57,7 @@ class G1_teacher extends MY_Controller {
                 $this->_data['subjects_list'][$key]['subject_years'][$k]['id'] = $subjectYear->subject_id;
                 
                 //$classes = $this->classes_model->get_classes_for_subject_year($subjectYear->subject_id, $subjectYear->year);
-                $classes = $this->classes_model->get_teacher_classes_years_subjects($this->session->userdata('id'),$subjectYear->subject_id, $subjectYear->year);
+                $classes = $this->subjects_model->get_teacher_classes_years_subjects($this->session->userdata('id'),$subjectYear->subject_id, $subjectYear->year);
 
                 foreach ($classes as $cl_key => $class) {
                     $this->_data['subjects_list'][$key]['subject_years'][$k]['classes'][$cl_key]['id'] = $class['id'];
