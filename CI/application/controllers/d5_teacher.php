5f9844ceb1cd0d8cdf1f6799cf20168e0130389f
mkdgo <dimitar.ovcharov70@gmail.com>
2015-10-26 13:24:05 +0200

    trello - issue #628

--- CI/application/controllers/d5_teacher.php
+++ CI/application/controllers/d5_teacher.php
@@ -79,9 +79,9 @@ class D5_teacher extends MY_Controller {
 
         if ($lesson_id != 0) {
             if ($interactive_content_exists > 0) {
-                $this->_data['create_edit_interactive_lesson'] = '<a href="/e1_teacher/index/' . $subject_id . '/' . $module_id . '/' . $lesson_id . '" class="red_btn">EDIT INTERACTIVE LESSON</a>';
+                $this->_data['create_edit_interactive_lesson'] = '<a href="/e1_teacher/index/' . $subject_id . '/' . $module_id . '/' . $lesson_id . '" class="red_btn">EDIT INTERACTIVE SLIDES</a>';
             } else {
-                $this->_data['create_edit_interactive_lesson'] = '<a href="/e1_teacher/index/' . $subject_id . '/' . $module_id . '/' . $lesson_id . '" class="red_btn">CREATE INTERACTIVE LESSON</a>';
+                $this->_data['create_edit_interactive_lesson'] = '<a href="/e1_teacher/index/' . $subject_id . '/' . $module_id . '/' . $lesson_id . '" class="red_btn">CREATE INTERACTIVE SLIDES</a>';
             }
         } else {
             $this->_data['create_edit_interactive_lesson'] = '';
