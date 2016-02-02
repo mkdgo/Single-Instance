0fc37ad1aea83ebc8421e1e84eb1669f48fecf70
mkdgo <dimitar.ovcharov70@gmail.com>
2016-02-02 12:28:18 +0200

    trello - issue #707

--- CI/application/controllers/d2_teacher.php
+++ CI/application/controllers/d2_teacher.php
@@ -87,9 +87,9 @@
                     $this->_data['modules'][$module_id]['lessons'][$lesson_id]['lesson_count'] = $i;
                     //$this->_data['modules'][$module_id]['lessons'][$lesson_id]['lesson_interactive'] = $this->interactive_content_model->if_has_assesments($lesson_id) > 0 ? '<div class="yesdot">YES</div>' : '<div class="nodot">NO</div>';
                     if ($this->lessons_model->interactive_lesson_published($lesson_id) > 0) {
-                        $slideicon = '<a href="/e1_teacher/index/'.$subject_id.'/'.$module_id.'/'.$lesson_id.'"><span class="circle"><span class="glyphicon glyphicon-ok"></span></span></a>';
+                        $slideicon = '<a href="/e1_teacher/index/'.$subject_id.'/'.$year_id.'/'.$module_id.'/'.$lesson_id.'"><span class="circle"><span class="glyphicon glyphicon-ok"></span></span></a>';
                     } elseif ($this->_data['modules'][$module_id]['lessons'][$lesson_id]['lesson_interactive'] = $this->interactive_content_model->if_has_assesments($lesson_id) > 0) {
-                        $slideicon = '<a href="/e1_teacher/index/'.$subject_id.'/'.$module_id.'/'.$lesson_id.'"><span class="circle" style=\'background-color: orange;\'><span class="glyphicon glyphicon-ok" style=\'background-color: orange;\'></span></span></a>';
+                        $slideicon = '<a href="/e1_teacher/index/'.$subject_id.'/'.$year_id.'/'.$module_id.'/'.$lesson_id.'"><span class="circle" style=\'background-color: orange;\'><span class="glyphicon glyphicon-ok" style=\'background-color: orange;\'></span></span></a>';
                     } else {
                         $slideicon = '';
                     }
