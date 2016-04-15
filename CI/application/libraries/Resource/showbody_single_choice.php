f7aee0a2292d5f156c8129bf37408d3d0d4966a3
ibeakoh <ibe@ediface.org>
2016-04-15 23:27:02 +0100

    Update showbody_single_choice.php

--- CI/application/libraries/Resource/showbody_single_choice.php
+++ CI/application/libraries/Resource/showbody_single_choice.php
@@ -1,7 +1,7 @@
 <style type="text/css">
 /*    .form-group { display: inline-block; float: left;}*/
 </style>
-<div id="single_choice" class="form-group grey resource_type no-margin" style="padding-top:21px;margin-left: 0; margin-right: 0;">
+<div id="single_choice" class="form-group form-group-question resource_type no-margin" style="padding-top:21px;margin-left: 0; margin-right: 0;">
     <div class="form-group form-group-question no-margin row" style="margin-left: 0; margin-right: 0; ">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <div class="" style="text-align: center;">[IMG]</div>
