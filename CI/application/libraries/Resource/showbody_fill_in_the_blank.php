4953c6aa8aa3e7a8b25e63cab248e3a977aa28a3
ibeakoh <ibe@ediface.org>
2016-04-15 23:27:29 +0100

    Update showbody_fill_in_the_blank.php

--- CI/application/libraries/Resource/showbody_fill_in_the_blank.php
+++ CI/application/libraries/Resource/showbody_fill_in_the_blank.php
@@ -1,37 +1,28 @@
 <style type="text/css">
 /*    .form-group { display: inline-block; float: left;}*/
 </style>
-<div id="fill_in_the_blank" class="form-group grey resource_type no-margin" style="padding-top:21px;margin-left: 0; margin-right: 0;">
-    <div class="form-group grey no-margin row" style="margin-left: 0; margin-right: 0; ">
+<div id="fill_in_the_blank" class="form-group form-group-question resource_type no-margin" style="padding-top:21px;margin-left: 0; margin-right: 0;">
+    <div class="form-group form-group-question no-margin row" style="margin-left: 0; margin-right: 0; ">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <div class="" style="text-align: center;">[IMG]</div>
         </div>
     </div>
 
-    <div class="form-group grey no-margin row" style="margin-left: 0; margin-right: 0; margin-bottom: 30px;">
-        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-            <label for="" class="scaled"></label>
-        </div>
-        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-            <div class="" style="text-align: left;"><label for="resource_link" class="scaled">[TEXT]</label></div>
+    <div class="form-group form-group-question no-margin row" style="margin-left: 0; margin-right: 0; margin-bottom: 30px;">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="introtext" style="text-align: center;"><label for="resource_link" class="scaled">[TEXT]</label></div>
         </div>
     </div>
 
-    <div class="form-group grey no-margin row" style="margin-left: 0; margin-right: 0; margin-bottom: 30px;">
-        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
-            <label for="" class="scaled" style="float: right;">Q:</label>
-        </div>
-        <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
-            <div class="" style="text-align: left; margin-left: 20px;"><label for="resource_link" class="scaled">[QUESTION]</label></div>
+    <div class="form-group form-group-question no-margin row" style="margin-left: 0; margin-right: 0; margin-bottom: 30px;">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="question" style="text-align: center;"><label for="resource_link" class="scaled">[QUESTION]</label></div>
         </div>
     </div>
 
-    <div class="form-group grey no-margin row" style="margin-left: 0; margin-right: 0;">
-        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
-            <label for="" class="scaled" style="float: right;">A:</label>
-        </div>
-        <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
-            <div class="" style="text-align: left; margin-left: 20px;"><label for="resource_link" class="scaled" style="line-height: 2.5;">[ANSWERS]</label></div>
+    <div class="form-group form-group-question no-margin row" style="margin-left: 0; margin-right: 0;">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="" style="text-align: center;"><label for="resource_link" class="scaled" style="line-height: 2.5;">[ANSWERS]</label></div>
         </div>
     </div>
 </div>
