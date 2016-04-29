3e29a48270dd1fb1a48d5997c02fd27c671e2ac6
ibeakoh <ibe@ediface.org>
2016-04-29 23:59:12 +0100

    Update showbody_mark_the_words.php

--- CI/application/libraries/Resource/showbody_mark_the_words.php
+++ CI/application/libraries/Resource/showbody_mark_the_words.php
@@ -8,13 +8,13 @@
         </div>
     </div>
 
-    <div class="form-group form-group-question no-margin row" style="margin-left: 0; margin-right: 0; margin-bottom: 30px;">
+    <div class="form-group form-group-question no-margin row" style="margin-left: 0; margin-right: 0;">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <div class="introtext" style="text-align: center;"><label for="resource_link" class="scaled">[TEXT]</label></div>
         </div>
     </div>
 
-    <div class="form-group form-group-question no-margin row" style="margin-left: 0; margin-right: 0; margin-bottom: 30px;">
+    <div class="form-group form-group-question no-margin row" style="margin-left: 0; margin-right: 0;">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <div class="question" style="text-align: center;"><label for="resource_link" class="scaled question">[QUESTION]</label></div>
         </div>
