ea326745e35f7ed92d4d25aca1c8f5154e498019
mkdgo <dimitar.ovcharov70@gmail.com>
2016-05-04 19:47:04 +0300

    i18 naming

---
+++ CI/application/language/english/c1_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['c1_resources'] = "Resources";
+$lang['c1_search_resources'] = "Search resources";
+/*$lang['c1_'] = 'You have successfully verified <a href="%s">%t</a> as your identity.';
+$lang['c1_'] = "<p>The following PAPE policies affected the authentication:</p><ul>";
+$lang['c1_'] = "<p>No PAPE response was sent by the provider.</p>";
+$lang['c1_'] = "<p>No PAPE policies affected the authentication.</p>";
+$lang['c1_'] = " Your %s is '%t'.";
+$lang['c1_'] = "  (XRI CanonicalID: %s) ";
+$lang['c1_'] = "<p>The NIST auth level returned by the server is: <tt>%s</tt></p>";
+$lang['c1_'] = "<p>The authentication age returned by the server is: <tt>%s</tt></p>";*/
+
+$lang['c1_btn_save'] = 'SAVE';
+$lang['c1_btn_new_resource'] = 'CREATE NEW RESOURCE';
+$lang['c1_btn_search'] = 'Search';
+/*$lang['c1_btn_'] = 'PAPE failed.';
+$lang['c1_btn_'] = 'Could not redirect to server: %s';
+$lang['c1_btn_'] = 'Could not create the FileStore directory %s. Please check the effective permissions.';*/
+
+?>
