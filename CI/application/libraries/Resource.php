f7e74cea3ee20bfadd664a7b745b42b4b85d6989
ibeakoh <ibe@ediface.org>
2016-05-12 23:08:32 +0100

    Update Resource.php

--- CI/application/libraries/Resource.php
+++ CI/application/libraries/Resource.php
@@ -768,7 +768,8 @@ class Resource {
                 if( count($answers_results) > 0 ) {
                     for( $a = 0; $a < count($answers_results); $a++ ) {
                         for( $c = 0; $c < (count( $answers_true )); $c++ ) {
-                            $arr_ans['rows'][0][$c+1] = 0;
+                            //$arr_ans['rows'][0][$c+1] = 0;
+                            $arr_ans['rows'][0][$answers_true['label']] = 0;
                         }
                     }
                 } else {
