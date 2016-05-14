6d136072771aac364d1d16551828a767e90cbc02
ibeakoh <ibe@ediface.org>
2016-05-15 00:11:29 +0100

    Update Resource.php

--- CI/application/libraries/Resource.php
+++ CI/application/libraries/Resource.php
@@ -698,10 +698,11 @@ class Resource {
                 $pos = array();
                 $i = 0;
                 foreach( $answers_true as $key => $ans ) {
-                    $arr[$i] = 0;
+                    //$arr[$i] = 0;
                     if( in_array( 'w'.$ans['position'], $answers_results ) ) {
                         $attained = $attained + $ans['value'];
                     } else {
+                        
                     }
                     $pos[] = 'w'.$ans['position'];
                 }
