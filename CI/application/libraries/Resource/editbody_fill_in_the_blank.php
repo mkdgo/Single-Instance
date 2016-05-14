0d2d6704963a34893d865d0d4d1771a21df0c10c
ibeakoh <ibe@ediface.org>
2016-05-14 17:27:40 +0100

    Update editbody_fill_in_the_blank.php

--- CI/application/libraries/Resource/editbody_fill_in_the_blank.php
+++ CI/application/libraries/Resource/editbody_fill_in_the_blank.php
@@ -22,7 +22,7 @@
 
     <div class="form-group form-group-question no-margin row" style="margin-left: 0; margin-right: 0;">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <div class="" style="text-align: center; "><label for="resource_link" class="scaled" style="line-height: 65px;margin: 50px;font-size: 19px !important;">[ANSWERS]</label></div>
+            <div class="" style="text-align: center; "><label for="resource_link" class="scaled" style="line-height: 65px;margin: 0px 50px 50px;font-size: 19px !important;">[ANSWERS]</label></div>
         </div>
     </div>
 </div>
