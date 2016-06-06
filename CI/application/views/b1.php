199643f828b087f27dd824586770b4110e4ab709
mkdgo <dimitar.ovcharov70@gmail.com>
2016-06-06 10:05:19 +0300

    trello - issue #841

--- CI/application/views/b1.php
+++ CI/application/views/b1.php
@@ -6,16 +6,18 @@
         <div class="student_in_wrapper">
             <div class="row align_center">
                 <div class="center_if_little w150" >
+                    <?php if( DEMO == 1 ): ?>
+                    <a href="/d1" class="my_classes"></a>
+                    <a class="student_text" href="javascript:;"><?php echo $lang['b1_my_classes'] ?></a>
+                    <?php else: ?>
                     <a href="javascript:;" class="my_classes" style="text-decoration: none; cursor: default; opacity: 0.5; outline: none;"></a>
                     <a class="student_text" href="javascript:;" style="text-decoration: none; cursor: default; opacity: 0.5; outline: none;"><?php echo $lang['b1_my_classes'] ?></a>
+                    <?php endif ?>
                 </div>
-                <div style="" class="center_if_little w150 " >
+                <div class="center_if_little w150 " >
                     <div style="display: {dot_hidden}; width: 32px; height: 32px; position: absolute; margin-left: 100px; background: #fff; border-radius: 50%; ">
                         <div style="background: <?php if( $late > 0 ) echo '#e74c3c'; else echo '#333'; ?>; border-radius: 50%; border-color:2px solid #fff; font-size: 16px; color: white; margin:2px 0 0 2px;width: 28px; height: 28px; padding-top: 2px;">{student_assignments}</div>
                     </div>
-<!--                    <div style="display: {dot_hidden}; width: 32px; height: 32px; position: absolute; margin-left: 120px; margin-top: 32px; background: #fff; border-radius: 50%; ">
-                        <div style="background: #333; border-radius: 50%; border-color:2px solid #fff; font-size: 16px; color: white; margin: 2px 0 0 2px;width: 28px; height: 28px;padding-top: 2px;">{student_assignments}</div>
-                    </div>-->
                     <a href="/f1_student"  class="my_homework_student"></a>
                     <a class="student_text" href="/f1_student"><?php echo $lang['b1_my_homework'] ?></a>
                 </div>
@@ -23,11 +25,14 @@
                     <a href="/c1" class="my_resources"></a>
                     <a class="student_text" href="/c1"><?php echo $lang['b1_my_resources'] ?></a>
                 </div>
-                <div style="" class="center_if_little w150 " >
+                <div class="center_if_little w150 " >
+                    <?php if( DEMO == 1 ): ?>
+                    <a href="/g1_student"  class="my_student_work"></a>
+                    <a class="student_text" href="/g1_student"><?php echo $lang['b1_my_work'] ?></a>
+                    <?php else: ?>
                     <a href="javascript:;" class="my_student_work" style="text-decoration: none; cursor: default; opacity: 0.5; outline: none;"></a>
                     <a class="student_text" href="javascript:;" style="text-decoration: none; cursor: default; opacity: 0.5; outline: none;"><?php echo $lang['b1_my_work'] ?></a>
-<!--                    <a href="/g1_student"  class="my_student_work"></a>
-                    <a class="student_text" href="/g1_student">My Work</a>  temporary unavailable-->
+                    <?php endif ?>
                 </div>                
             </div>
         </div>
