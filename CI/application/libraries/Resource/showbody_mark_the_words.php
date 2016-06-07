feaaf2e990966e161a2eb860354c52807e5e0099
mkdgo <dimitar.ovcharov70@gmail.com>
2016-06-07 18:36:44 +0300

    trello - issue #732

--- CI/application/libraries/Resource/showbody_mark_the_words.php
+++ CI/application/libraries/Resource/showbody_mark_the_words.php
@@ -24,7 +24,7 @@
     function setAnswer(el, w, c) {
         var allowed = parseInt($('#q'+c+'_c').attr('rel'));
         var marked = parseInt($('#q'+c+'_c').attr('num'));
-        $('#'+w).val( $('#'+w).attr('rel') );
+
         if( el.attr('rel') == 0 ) {
             if( marked >= allowed ) {
                $('.modal-body').html('').append('<div class="alert-error" style=" margin: 10px 20px; line-height: 1.5;">You have made the maximum number of selections. Please deselect one of the words to make another selection.</div>');
@@ -34,11 +34,13 @@
             }
             el.attr('rel', 1);
             el.css('background', '#53EEEB');
+            $('#'+w).val( $('#'+w).attr('rel') );
             marked = marked + 1;
             $('#q'+c+'_c').attr('num',marked);
         } else {
             el.attr('rel', 0);
             el.css('background', '#f5f5f5');
+            $('#'+w).val('');
             marked = marked - 1;
             $('#q'+c+'_c').attr('num',marked);
         }
